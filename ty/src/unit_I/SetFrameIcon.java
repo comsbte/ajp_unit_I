66bf56cb5e537bc1c30962e3322ddba9f956bf39
sparutwar <sparutwar@gmail.com>
2022-09-13 15:21:44 +0530

    added size

--- ty/src/unit_I/SetFrameIcon.java
+++ ty/src/unit_I/SetFrameIcon.java
@@ -5,17 +5,28 @@ import java.awt.Font;
 import java.awt.Frame;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class SetFrameIcon {
 	public static void main(String[] args) {
 		Frame f = new Frame("Test");
-		f.setVisible(true);
+		
 		MyButton2 obj = new MyButton2();
 		Font font1 = new Font("Serif", Font.PLAIN,48);
 		Image i = Toolkit.getDefaultToolkit().getImage("C:\\Users\\arutwar\\Pictures\\logo_msbte.png");
 		f.setIconImage(i);
+		f.setSize(400, 400);
 		obj.setFont(font1);
 		f.add(obj);
+		f.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				f.dispose();
+			}
+		});
+		
+		f.setVisible(true);
 	}
 }
 
